9e01463d2e87abea6083fd74db1a929bd2d6f0ed
WouterRademaker <egel@scoutnet.nl>
2021-09-15 20:03:23 +0200

    small bug and start test page

--- EPSG.i18n.magic.php
+++ EPSG.i18n.magic.php
@@ -11,6 +11,6 @@ $magicWords = array();
 /** English (English) */
 $magicWords['en'] = array(
    'wgs84_2epsg'    => array( 0, 'wgs84_2epsg'),
-   'epsg_2wgs84'    => array( 0, 'wgs84_2epsg'),
+   'epsg_2wgs84'    => array( 0, 'epsg_2wgs84'),
    'epsg'           => array( 0, 'epsg'),
 );
--- specials/SpecialEPSGTest.php
+++ specials/SpecialEPSGTest.php
@@ -30,7 +30,18 @@ class SpecialEPSGTest extends SpecialPage {
 
 		# Do stuff
 		# ...
-		$wikitext = '    ';
+		$wikitext = '
+This should give	x = 120700.723 m y = 487525.501 m
+*{{#epsg:4.88352559,52.37453253,0||7415}}
+*{{#epsg:4.88352559,52.37453253,0|4326|7415}}
+*{{#epsg:4.88352559,52.37453253||7415}}
+*{{#epsg:4.88352559,52.37453253|4326|7415}}
+*{{#wgs84_2epsg:52.37453253,4.88352559,0|7415}}
+*{{#wgs84_2epsg:52.37453253,4.88352559|7415}}
+
+*{{#epsg:120700,487525,-43|7415}} gives, approximately: 4.88351, 52.3745, 0
+*{{#epsg_2wgs84:120700,487525,-43|7415}} gives, approximately: 52.3745, 4.88351, 0
+   ';
     		$out->addWikiTextAsInterface( $wikitext );
     	}
 
